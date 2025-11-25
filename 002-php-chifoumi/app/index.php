cf831d56984e73eb6b0917098928ea573ba176f4
YoannDeStena <ydestena@gmail.com>
2025-11-25 15:43:07 +0100

    add: Ajout de l'entièreté de l'exercice chifoumi

--- 002-php-chifoumi/app/index.php
+++ 002-php-chifoumi/app/index.php
@@ -1,3 +1,50 @@
 <?php
 
-echo "Hello world";
+$hasPlayer = isset($_GET['player']);
+$player = $_GET["player"] ?? "Choississez un attaquant";
+$choices = ["Pierre", "Feuille", "Ciseaux"];
+$phpPlayer = $choices[array_rand($choices)];
+$result = "Aucun";
+
+if($hasPlayer) {
+    if ($player == $phpPlayer) {
+        $result = "Égalité";
+    } elseif
+    ($player == "Pierre" && $phpPlayer == "Ciseaux" ||
+        $player == "Feuille" && $phpPlayer == "Pierre" ||
+        $player == "Ciseaux" && $phpPlayer == "Feuille") {
+        $result = "Gagné";
+    }
+}
+
+
+$html = <<<HTML
+<!doctype html>
+    <html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Pierre, Feuille, Ciseaux </title>
+        <link type="text/css" rel="stylesheet" href="style.css">
+        
+    </head>
+    <body>
+        <h1>Jeu Pierre, Feuille, Ciseaux</h1>
+        <div class="row">
+            <h2>Joueur : $player</h2>
+            <h2>PHP : $phpPlayer</h2>
+            <h2>Resultat : $result</h2>
+        </div>
+        <div class="row">
+            <a href="index.php?player=Pierre"><button type="submit" >Pierre</button></a>
+            <a href="index.php?player=Feuille"><button type="submit">Feuille</button></a>
+            <a href="index.php?player=Ciseaux"><button type="submit">Ciseaux</button></a>
+        </div>
+        <div class="row">
+            <a href="index.php"><button type="submit">Reset</button></a>
+        </div>
+    </body>
+    </html>
+HTML;
+
+echo $html;
