d5f7703284c874721c66600e53e77a6f87c80f5b
YoannDeStena <ydestena@gmail.com>
2025-11-27 11:10:12 +0100

    add (004): Ajout d'une page artistes.php basique avec du style

---
+++ 004-php-lowify/app/artists.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once 'inc/page.inc.php';
+require_once 'inc/database.inc.php';
+
+$artists = (new DatabaseManager(
+    "mysql:host=mysql;dbname=lowify;charset=utf8mb4",
+    "lowify",
+    "lowifypassword"))->executeQuery("SELECT * FROM artist");
+
+$htmlArtists = "<div class='row'>";
+
+foreach ($artists as $artist) {
+    $htmlArtists =  $htmlArtists . "<div class='imageblock'>";
+    $htmlArtists = $htmlArtists . $artist["name"];
+    $htmlArtists = $htmlArtists . "<img src=\"" . $artist["cover"] . "\">";
+    $htmlArtists = $htmlArtists . "<br>" . $artist["biography"];
+    $htmlArtists =  $htmlArtists . "</div>";
+}
+
+$htmlArtists =  $htmlArtists . "</div>";
+
+$html = <<<HTML
+    <h1>Artistes<br></h1>
+    $htmlArtists
+HTML;
+
+echo (new HTMLPage("Lowify - Artistes"))->addContent($html)->addStylesheet("style.css")->render();
