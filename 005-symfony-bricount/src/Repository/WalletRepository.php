dee54d1470a18b07b2020e7b1723a83d879912d2
YoannDeStena <ydestena@gmail.com>
2025-12-16 15:02:47 +0100

    add (005): Ajout de la possibilité des trouver toutes les Wallets d'un usr depuis WalletRepository

--- 005-symfony-bricount/src/Repository/WalletRepository.php
+++ 005-symfony-bricount/src/Repository/WalletRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\Wallet;
+use App\Entity\XUserWallet;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -14,5 +15,15 @@ class WalletRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Wallet::class);
+    }
+
+    public function findWalletsForUser(User $user):array
+    {
+        $qb = $this
+            ->createQueryBuilder('w')
+            ->innerJoin(XUserWallet::class, 'xuw', 'WITH', 'xuw.wallet = w.id AND xuw.isDeleted = false AND xuw.targetUser = :user')
+            ->andWhere('w.isDeleted = false')
+            ->setParameter('user', $user);
+        return $qb->getQuery()->getResult();
     }
 }
