ddfa4c20435c7a72037c44a773fefbe85649bb48
YoannDeStena <ydestena@gmail.com>
2025-12-18 11:42:43 +0100

    add (005): Ajout de la requête pour trouver le XUserWallet d'un User et son Wallet

--- 005-symfony-bricount/src/Repository/XUserWalletRepository.php
+++ 005-symfony-bricount/src/Repository/XUserWalletRepository.php
@@ -2,6 +2,8 @@
 
 namespace App\Repository;
 
+use App\Entity\User;
+use App\Entity\Wallet;
 use App\Entity\XUserWallet;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
@@ -16,28 +18,14 @@ class XUserWalletRepository extends ServiceEntityRepository
         parent::__construct($registry, XUserWallet::class);
     }
 
-    //    /**
-    //     * @return XUserWallet[] Returns an array of XUserWallet objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('x')
-    //            ->andWhere('x.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('x.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?XUserWallet
-    //    {
-    //        return $this->createQueryBuilder('x')
-    //            ->andWhere('x.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
+    public function getUserAccessOnWallet(User $user, Wallet $wallet): null|XUserWallet {
+        return $this
+            ->createQueryBuilder("xuw")
+            ->innerJoin("xuw.targetUser", "u", "WITH", "u.id = :userId")
+            ->innerJoin("xuw.wallet", "w", "WITH", "w.isDeleted = false AND w.id = :walletId")
+            ->andWhere("xuw.isDeleted = false")
+            ->setParameter("userId", $user->getId())
+            ->setParameter("walletId", $wallet->getId())
+            ->getQuery()->getOneOrNullResult();
+    }
 }
--- 005-symfony-bricount/src/Service/WalletService.php
+++ 005-symfony-bricount/src/Service/WalletService.php
@@ -3,16 +3,23 @@
 namespace App\Service;
 
 use App\Entity\User;
+use App\Entity\Wallet;
+use App\Entity\XUserWallet;
 use App\Repository\WalletRepository;
+use App\Repository\XUserWalletRepository;
 
 class WalletService
 {
-    public function __construct(private readonly WalletRepository $walletRepository)
+    public function __construct(private readonly WalletRepository $walletRepository, private readonly XUserWalletRepository $XUserWalletRepository)
     {
 
     }
 
     public function findWalletsForUser(User $user): array {
         return $this->walletRepository->findWalletsForUser($user);
+    }
+
+    public function getUserAccessOnWallet(User $user, Wallet $wallet): null|XUserWallet {
+        return $this->XUserWalletRepository->getUserAccessOnWallet($user, $wallet);
     }
 }
